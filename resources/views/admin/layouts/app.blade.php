50321d9e3dc5ecb1337ab5e646ac66552138d5c6
davyalex <alexkouamelan96pv@gmail.com>
2023-11-27 23:21:18 +0000

    optimize

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -7,6 +7,13 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{config('app.name')}} - Admin Dashboard </title>
+
+      <!-- Favicons -->
+      <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+      <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/images/favicon/apple-touch-icon.png') }}">
+      <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/favicon/favicon-32x32.png') }}">
+      <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicon/favicon-16x16.png') }}">
+  
     <!-- General CSS Files -->
     {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.js"></script> --}}
 
