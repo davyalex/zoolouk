41092e5de3179c359b13b8e3deeaca5e730c0ff3
davyalex <alexkouamelan96pv@gmail.com>
2023-12-02 18:55:18 +0000

    optimize

--- resources/views/site/pages/shop.blade.php
+++ resources/views/site/pages/shop.blade.php
@@ -8,8 +8,8 @@
     <style>
         .adapted-img {
             width: auto;
-            height:150px;
-            object-fit: cover;
+            height:130px;
+            object-fit: contain;
             /* margin-bottom: -10px; */
 
         }
@@ -52,7 +52,7 @@
                     @foreach ($product as $item)
                         <div class="col img-div">
                             <div class="card rounded-3 border-1" style="width: 100%;height:100% ">
-                                <div class="position-relative overflow-hidden" style="width: 100%;height:150px ">
+                                <div class="position-relative overflow-hidden">
                                     <a href="{{ route('product-detail', $item['id']) }}">
                                         <img src="{{ $item->getFirstMediaUrl('product_image') }}"
                                             class="img-fluid  rounded-top-3 adapted-img" alt="...">
