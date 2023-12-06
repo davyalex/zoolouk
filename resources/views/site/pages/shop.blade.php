e4cbe5b5eed112dcf42c4175d1d3f02f94598bf8
davyalex <alexkouamelan96pv@gmail.com>
2023-12-06 23:40:47 +0000

    optimize

--- resources/views/site/pages/shop.blade.php
+++ resources/views/site/pages/shop.blade.php
@@ -82,21 +82,24 @@
                             </div>
                         </div>
                     @endforeach
+                    <div class="py-3 m-auto">
+                        {{-- @if ($product->count() > 0) --}}
+                            
+                        {!! $product->appends(request()->query())->links('pagination::bootstrap-4') !!}
+                        {{-- @endif --}}
+                @endif
                 @else
                     <h2 class="text-center mt-5 w-100">Pas de produit disponible
                         <br><small class="m-auto text-secondary fs-6">Pour la categorie ou votre recherche</small>
                     </h2>
-                @endif
+
+                 
 
 
             </div><!--end row-->
         </div>
         <!--end produt grid-->
-        <div class="py-3 m-auto">
-            @if ($product->count() > 0)
-                
-            {!! $product->appends(request()->query())->links('pagination::bootstrap-4') !!}
-            @endif
+       
 
         </div>
         <div class="py-2"></div>
