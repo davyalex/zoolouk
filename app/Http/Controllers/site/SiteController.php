c7645f5555b4bf52db690e1f5e9a3c48f5813b04
davyalex <alexkouamelan96pv@gmail.com>
2023-12-07 17:08:34 +0000

    optimize

--- app/Http/Controllers/site/SiteController.php
+++ app/Http/Controllers/site/SiteController.php
@@ -42,7 +42,7 @@ class SiteController extends Controller
 
         //category with product
         $category_with_product = Category::withWhereHas('products', fn ($q) =>
-        $q->with('media'))->orderBy('type','DESC')->get();
+        $q->with('media'))->orderBy('type','ASC')->get();
 
         //subcategory with product
         $subcategory_with_product = SubCategory::withWhereHas('products', fn ($q) =>
