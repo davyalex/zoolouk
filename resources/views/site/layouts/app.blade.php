ffd725dad8a9dedc1651b98ad6346c039ca57051
davyalex <alexkouamelan96pv@gmail.com>
2023-12-08 09:38:49 +0000

    optimize

--- resources/views/site/layouts/app.blade.php
+++ resources/views/site/layouts/app.blade.php
@@ -47,14 +47,14 @@
 <body>
 
     <!--page loader-->
-    {{-- <div class="loader-wrapper">
+    <div class="loader-wrapper">
         <div
             class="d-flex justify-content-center align-items-center position-absolute top-50 start-50 translate-middle">
             <div class="spinner-border text-white" role="status">
                 <span class="visually-hidden">Loading...</span>
             </div>
         </div>
-    </div> --}}
+    </div>
     <!--end loader-->
 
     <!--start wrapper-->
